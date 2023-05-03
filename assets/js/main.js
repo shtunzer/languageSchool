a334cf54ecd6b9d10b7d660b765882430374c010
Denis <130508415+shtunzer@users.noreply.github.com>
2023-05-03 23:26:32 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+document.getElementById('burger-btn-open').addEventListener('click', function(){
+    document.getElementById('burger__menu').classList.add('--show');
+});
+document.getElementById('burger-btn-hide').addEventListener('click', function(){
+    document.getElementById('burger__menu').classList.remove('--show');
+});
+
 // SWIPER
 
 var swiper = new Swiper(".mySwiper", {
